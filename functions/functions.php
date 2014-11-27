1e04d50730b5e1c4f449f78fcfdef44bfc663c59
myha <myha@d9489115-519a-4661-88eb-0d2216f09706>
2014-11-27 09:26:18 +0000

    Fixed unable to edit users | v 1.07.006
    git-svn-id: http://svn.code.sf.net/p/phpipam/code@466 d9489115-519a-4661-88eb-0d2216f09706

--- functions/functions.php
+++ functions/functions.php
@@ -54,7 +54,7 @@ $database = new database($db['host'], $db['user'], $db['pass'], $db['name'], NUL
 /* set latest version */
 define("VERSION", "1.07");									//version changes if database structure changes
 /* set latest revision */
-define("REVISION", "005");									//revision always changes, verision only if database structure changes
+define("REVISION", "006");									//revision always changes, verision only if database structure changes
 /* set last possible upgrade */
 define("LAST_POSSIBLE", "0.9");								//minimum required version to be able to upgrade
 
